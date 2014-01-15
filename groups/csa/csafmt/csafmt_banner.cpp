fd267a9f2204559b07a210da062ea30b5ef23aba
Hyman Rosen <hrosen4@bloomberg.net>
2014-01-14 20:51:51 -0500

    Don't do full file scan for banners.

--- groups/csa/csafmt/csafmt_banner.cpp
+++ groups/csa/csafmt/csafmt_banner.cpp
@@ -17,6 +17,7 @@ static std::string const check_name("banner");
 
 // ----------------------------------------------------------------------------
 
+using clang::FileID;
 using clang::FixItHint;
 using clang::SourceLocation;
 using clang::SourceManager;
@@ -30,6 +31,33 @@ using cool::csabase::Visitor;
 namespace
 {
 
+struct comments
+    // Data holding seen comments.
+{
+    typedef std::map<std::string, std::vector<SourceRange> > Comments;
+    Comments d_comments;
+
+    void append(Analyser& analyser, SourceRange range);
+};
+
+void comments::append(Analyser& analyser, SourceRange range)
+{
+    SourceManager& m = analyser.manager();
+    std::vector<SourceRange>& c = d_comments[m.getFilename(range.getBegin())];
+    if (c.size() != 0 &&
+        m.getFilename(c.back().getEnd()) == m.getFilename(range.getBegin()) &&
+        m.getPresumedLineNumber(range.getBegin()) ==
+        m.getPresumedLineNumber(c.back().getEnd()) + 1 &&
+        analyser.get_source(SourceRange(c.back().getEnd(),
+                                        range.getBegin().getLocWithOffset(-1)),
+                            true).find_first_not_of(" \t\n\r\v\f") ==
+            llvm::StringRef::npos) {
+        c.back().setEnd(range.getEnd());
+    } else {
+        c.push_back(range);
+    }
+}
+
 struct files
     // Callback object for inspecting files.
 {
@@ -38,10 +66,11 @@ struct files
     files(Analyser& analyser);
         // Create a 'files' object, accessing the specified 'analyser'.
 
-    void operator()(SourceLocation     loc,
-                    std::string const &from,
-                    std::string const &file);
-        // The file specified by 'loc' is examined for improper banners.
+    void operator()(SourceRange range);
+        // The specified comment 'range' is added to the stored data.
+
+    void operator()();
+        // Report improper banners.
 };
 
 files::files(Analyser& analyser)
@@ -49,70 +78,65 @@ files::files(Analyser& analyser)
 {
 }
 
-void files::operator()(SourceLocation     loc,
-                       std::string const &from,
-                       std::string const &file)
+void files::operator()(SourceRange range)
 {
-    if (!d_analyser.is_component(file)) {
-        return;                                                       // RETURN
-    }
+    d_analyser.attachment<comments>().append(d_analyser, range);
+}
 
-    const SourceManager &m = d_analyser.manager();
-    llvm::StringRef buf = m.getBufferData(m.getFileID(loc));
-
-    static const std::string sep = "\n// " + std::string(76, '=') + "\n";
-    static const std::string inline_banner_expect = // correct banner
-        sep +
-        "//" + std::string(25, ' ') + "INLINE FUNCTION DEFINITIONS" +
-        sep;
+static const std::string sep = "// " + std::string(76, '=');
+static const std::string inline_banner_expect =  // correct banner
+    sep + "\n//" + std::string(25, ' ') + "INLINE FUNCTION DEFINITIONS\n" + sep;
 
 #undef NL
 #define NL "\r*[\r\n]"
 #undef SP
 #define SP "[ \t]*"
 
-    static llvm::Regex loose_inline_banner(    // match sloppy banner
-                                                                            NL
-            SP "//" SP "[-=]*"  SP                                          NL
-            SP "//" SP "INLINE"
-                    SP "(" "FUNCTIONS?"   SP ")?"
-                    SP "(" "DEFINITIONS?" SP ")?"                           NL
-            SP "//" SP "[-=]*"  SP                                          NL,
-        llvm::Regex::IgnoreCase);
+static llvm::Regex loose_inline_banner(    // match sloppy banner
+    SP "//" SP "[-=]*"  SP                NL
+    SP "//" SP "INLINE"
+            SP "(" "FUNCTIONS?"   SP ")?"
+            SP "(" "DEFINITIONS?" SP ")?" NL
+    SP "//" SP "[-=]*"  SP,
+    llvm::Regex::IgnoreCase);
 
-    static llvm::Regex tight_inline_banner(    // match correct banner
-                                                                            NL
-            "// ={76}"                                                      NL
-            "// {25}INLINE FUNCTION DEFINITIONS"                            NL
-            "// ={76}"                                                      NL,
-        llvm::Regex::NoFlags);
+static llvm::Regex tight_inline_banner(    // match correct banner
+    "// ={76}"                            NL
+    "// {25}INLINE FUNCTION DEFINITIONS"  NL
+    "// ={76}",
+    llvm::Regex::NoFlags);
 
 #undef SP
 #undef NL
 
-    llvm::SmallVector<llvm::StringRef, 3> matches;
-    size_t offset = 0;
-    size_t end    = 0;
-
-    while (loose_inline_banner.match(buf.drop_front(end), &matches)) {
-        llvm::StringRef match = matches[0];
-        offset = end + buf.drop_front(end).find(match);
-        end    = offset + match.size();
-
-        if (!tight_inline_banner.match(buf.substr(offset, end - offset))) {
-            std::pair<unsigned, unsigned> m =
-                cool::csabase::mid_mismatch(match, inline_banner_expect);
-            SourceRange r(loc.getLocWithOffset(offset + m.first),
-                          loc.getLocWithOffset(end    - m.second));
-            d_analyser.report(r.getBegin(),
-                              check_name, "BAN01",
-                              "Inline banner with incorrect form "
-                              "is %0 and should be %1")
-                << match
-                << inline_banner_expect
-                << FixItHint::CreateReplacement(r, inline_banner_expect.substr(
-                            m.first,
-                            inline_banner_expect.size() - m.first - m.second));
+void files::operator()()
+{
+    comments::Comments& c = d_analyser.attachment<comments>().d_comments;
+    for (comments::Comments::iterator itr = c.begin(); itr != c.end(); ++itr) {
+        if (d_analyser.is_component(itr->first)) {
+            std::vector<SourceRange>& b = itr->second;
+            size_t n = b.size();
+            for (size_t i = 0; i < n; ++i) {
+                llvm::StringRef s = d_analyser.get_source(b[i], true);
+                if (loose_inline_banner.match(s) &&
+                    !tight_inline_banner.match(s)) {
+                    std::pair<unsigned, unsigned> m =
+                        cool::csabase::mid_mismatch(s, inline_banner_expect);
+                    SourceRange r(b[i].getBegin().getLocWithOffset(m.first),
+                                  b[i].getEnd().getLocWithOffset(m.second));
+                    d_analyser.report(r.getBegin(), check_name, "BAN01",
+                                      "Inline banner with incorrect form "
+                                      "is\n%0\nand should be\n%1")
+                        << s
+                        << inline_banner_expect
+                        << FixItHint::CreateReplacement(
+                               r,
+                               inline_banner_expect.substr(
+                                   m.first,
+                                   inline_banner_expect.size() - m.first -
+                                       m.second));
+                }
+            }
         }
     }
 }
@@ -120,7 +144,8 @@ void files::operator()(SourceLocation     loc,
 void subscribe(Analyser& analyser, Visitor&, PPObserver& observer)
     // Hook up the callback functions.
 {
-    observer.onOpenFile += files(analyser);
+    analyser.onTranslationUnitDone += files(analyser);
+    observer.onComment             += files(analyser);
 }
 
 }  // close anonymous namespace
