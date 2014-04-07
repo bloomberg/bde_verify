cd6ae7e0597acc70fc2e5c8aa3eff2eb282b8e76
Hyman Rosen <hrosen4@bloomberg.net>
2014-04-07 19:13:19 -0400

    Try to catch all instances of typename vs. class.

--- groups/csa/csastil/csastil_templatetypename.cpp
+++ groups/csa/csastil/csastil_templatetypename.cpp
@@ -6,12 +6,14 @@
 // ----------------------------------------------------------------------------
 
 #include <csabase_analyser.h>
+#include <csabase_util.h>
 #include <csabase_registercheck.h>
-#include <csabase_cast_ptr.h>
 #include <llvm/Support/Regex.h>
+#include <clang/AST/ExprCXX.h>
 #include <clang/Rewrite/Core/Rewriter.h>
 
 using namespace clang;
+using namespace clang::ast_matchers;
 using namespace bde_verify::csabase;
 
 // ----------------------------------------------------------------------------
@@ -20,61 +22,142 @@ static std::string const check_name("template-typename");
 
 // ----------------------------------------------------------------------------
 
+namespace
+{
+
+struct data
+{
+    data();
+};
+
+data::data()
+{
+};
+
+struct report
+{
+    report(Analyser& analyser);
+    void match_has_template_parameters(const BoundNodes& nodes);
+    void checkTemplateParameters(TemplateParameterList const* parms);
+    void operator()();
+
+    Analyser& d_analyser;
+    data& d_data;
+};
+
+report::report(Analyser& analyser)
+: d_analyser(analyser)
+, d_data(analyser.attachment<data>())
+{
+}
+
+const internal::DynTypedMatcher &
+has_template_parameters_matcher()
+    // Return an AST matcher which looks for declarations.
+{
+    static const internal::DynTypedMatcher matcher =
+        decl(eachOf(
+            forEachDescendant(decl().bind("decl")),
+            forEachDescendant(lambdaExpr().bind("lambda"))
+        ));
+    return matcher;
+}
+
+void report::match_has_template_parameters(const BoundNodes& nodes)
+{
+    if (TemplateDecl const* decl = nodes.getNodeAs<TemplateDecl>("decl")) {
+        checkTemplateParameters(decl->getTemplateParameters());
+    }
+    if (DeclaratorDecl const* decl = nodes.getNodeAs<DeclaratorDecl>("decl")) {
+        unsigned n = decl->getNumTemplateParameterLists();
+        for (unsigned i = 0; i < n; ++i) {
+            checkTemplateParameters(decl->getTemplateParameterList(i));
+        }
+    }
+    if (TagDecl const* decl = nodes.getNodeAs<TagDecl>("decl")) {
+        unsigned n = decl->getNumTemplateParameterLists();
+        for (unsigned i = 0; i < n; ++i) {
+            checkTemplateParameters(decl->getTemplateParameterList(i));
+        }
+    }
+    if (ClassTemplatePartialSpecializationDecl const* decl =
+            nodes.getNodeAs<ClassTemplatePartialSpecializationDecl>("decl")) {
+        checkTemplateParameters(decl->getTemplateParameters());
+    }
+    if (VarTemplatePartialSpecializationDecl const* decl =
+            nodes.getNodeAs<VarTemplatePartialSpecializationDecl>("decl")) {
+        checkTemplateParameters(decl->getTemplateParameters());
+    }
+    if (CXXRecordDecl const* decl = nodes.getNodeAs<CXXRecordDecl>("decl")) {
+        checkTemplateParameters(decl->getGenericLambdaTemplateParameterList());
+    }
+    if (FriendDecl const* decl = nodes.getNodeAs<FriendDecl>("decl")) {
+        unsigned n = decl->getFriendTypeNumTemplateParameterLists();
+        for (unsigned i = 0; i < n; ++i) {
+            checkTemplateParameters(
+                decl->getFriendTypeTemplateParameterList(i));
+        }
+    }
+    if (TemplateTemplateParmDecl const* decl =
+            nodes.getNodeAs<TemplateTemplateParmDecl>("decl")) {
+        unsigned n = decl->getNumExpansionTemplateParameters();
+        for (unsigned i = 0; i < n; ++i) {
+            checkTemplateParameters(decl->getExpansionTemplateParameters(i));
+        }
+    }
+    if (LambdaExpr const* expr = nodes.getNodeAs<LambdaExpr>("lambda")) {
+        checkTemplateParameters(expr->getTemplateParameterList());
+    }
+}
+
+void report::operator()()
+{
+    MatchFinder mf;
+    OnMatch<report, &report::match_has_template_parameters> m1(this);
+    mf.addDynamicMatcher(has_template_parameters_matcher(), &m1);
+    mf.match(*d_analyser.context()->getTranslationUnitDecl(),
+             *d_analyser.context());
+}
+
 static llvm::Regex all_upper("^[[:upper:]](_?[[:upper:][:digit:]]+)*$");
 
-static void
-checkTemplateParameters(Analyser& analyser, TemplateParameterList const* parms)
+void report::checkTemplateParameters(TemplateParameterList const* parms)
 {
-    unsigned n = parms->size();
+    unsigned n = parms ? parms->size() : 0;
     for (unsigned i = 0; i < n; ++i) {
         const TemplateTypeParmDecl *parm =
             llvm::dyn_cast<TemplateTypeParmDecl>(parms->getParam(i));
         if (parm) {
             if (parm->wasDeclaredWithTypename()) {
-                 analyser.report(parm, check_name, "TY01",
+                 d_analyser.report(parm, check_name, "TY01",
                      "Template parameter uses 'typename' instead of 'class'");
                  SourceRange r = parm->getSourceRange();
-                 llvm::StringRef s = analyser.get_source(r);
+                 llvm::StringRef s = d_analyser.get_source(r);
                  size_t to = s.find("typename");
                  if (to != s.npos) {
-                     analyser.rewriter().ReplaceText(
+                     d_analyser.rewriter().ReplaceText(
                         r.getBegin().getLocWithOffset(to), 8, "class");
                  }
             }
             if (parm->getName().size() == 1) {
-                 analyser.report(parm, check_name, "TY02",
+                 d_analyser.report(parm, check_name, "TY02",
                      "Template parameter uses single-letter name");
             }
             if (!all_upper.match(parm->getName())) {
-                 analyser.report(parm, check_name, "TY03",
+                 d_analyser.report(parm, check_name, "TY03",
                      "Template parameter is not in ALL_CAPS format");
             }
         }
     }
 }
 
-static void
-check_t(Analyser& analyser, TemplateDecl const* decl)
+void subscribe(Analyser& analyser, Visitor&, PPObserver&)
 {
-    checkTemplateParameters(analyser, decl->getTemplateParameters());
+    analyser.onTranslationUnitDone += report(analyser);
 }
 
-static void
-check_ctps(Analyser& analyser,
-              ClassTemplatePartialSpecializationDecl const* decl)
-{
-    checkTemplateParameters(analyser, decl->getTemplateParameters());
-}
-
-static void
-check_vtps(Analyser& analyser,
-              VarTemplatePartialSpecializationDecl const* decl)
-{
-    checkTemplateParameters(analyser, decl->getTemplateParameters());
 }
 
 // ----------------------------------------------------------------------------
 
-static RegisterCheck c1(check_name, &check_t);
-static RegisterCheck c2(check_name, &check_ctps);
-static RegisterCheck c3(check_name, &check_vtps);
+static RegisterCheck c1(check_name, &subscribe);
--- groups/csa/csastil/csastil_templatetypename.t.cpp
+++ groups/csa/csastil/csastil_templatetypename.t.cpp
@@ -21,5 +21,51 @@ namespace bde_verify
             template <class TT>
             static void localGood();
         }
+
+        template <typename AA>
+        struct BB
+        {
+            template <typename CC>
+            void f();
+        };
     }
 }
+
+template <typename YY>
+template <typename CC>
+void bde_verify::csastil::BB<YY>::f()
+{
+}
+
+namespace AA {
+namespace BB {
+    template <typename CC>
+    struct DD {
+        template <typename EE>
+        void f();
+    };
+}
+}
+
+template <typename GG>
+template <typename HH>
+void AA::BB::DD<GG>::f()
+{
+}
+
+namespace II {
+namespace JJ {
+    template <typename KK>
+    struct LL
+    {
+        template <typename MM>
+        struct NN;
+    };
+}
+}
+
+template <typename OO>
+template <typename PP>
+struct II::JJ::LL<OO>::NN
+{
+};
