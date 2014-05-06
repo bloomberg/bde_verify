703d919ba1856194c3e3b7fafe63c1d569d8c8ac
Hyman Rosen <hrosen4@bloomberg.net>
2014-05-06 19:15:20 -0400

    Use variadic templates.

--- utils/event.hpp
+++ utils/event.hpp
@@ -17,223 +17,36 @@
 
 namespace utils
 {
-    template <typename Signature> class event;
-    template <>                                                   class event<void()>;
-    template <typename T0>                                        class event<void(T0)>;
-    template <typename T0, typename T1>                           class event<void(T0, T1)>;
-    template <typename T0, typename T1, typename T2>              class event<void(T0, T1, T2)>;
-    template <typename T0, typename T1, typename T2, typename T3> class event<void(T0, T1, T2, T3)>;
-}
 
-// -----------------------------------------------------------------------------
+template <typename Signature> class event;
 
-template <>
-class utils::event<void()>
+template <typename ...T>
+class event<void(T...)>
 {
-public:
-    template <typename Functor> utils::event<void()>& operator+= (Functor functor);
-    void operator()() const;
-    operator bool() const;
+  public:
+    template <typename Functor>
+    event& operator+=(Functor functor)
+    {
+        functions_.push_back(function<void(T...)>(functor));
+        return *this;
+    }
+
+    void operator()(T...a) const
+    {
+        for (const auto &f : functions_) {
+            f(a...);
+        }
+    }
+
+    operator bool() const
+    {
+        return !functions_.empty();
+    }
 
 private:
-    std::deque<utils::function<void()> > functions_;
+    std::deque<function<void(T...)>> functions_;
 };
 
-// -----------------------------------------------------------------------------
-
-template <typename T0>
-class utils::event<void(T0)>
-{
-public:
-    template <typename Functor> utils::event<void(T0)>& operator+= (Functor functor);
-    void operator()(T0) const;
-    operator bool() const;
-
-private:
-    std::deque<utils::function<void(T0)> > functions_;
-};
-
-// -----------------------------------------------------------------------------
-
-template <typename T0, typename T1>
-class utils::event<void(T0, T1)>
-{
-public:
-    template <typename Functor> utils::event<void(T0, T1)>& operator+= (Functor functor);
-    void operator()(T0, T1) const;
-    operator bool() const;
-
-private:
-    std::deque<utils::function<void(T0, T1)> > functions_;
-};
-
-// -----------------------------------------------------------------------------
-
-template <typename T0, typename T1, typename T2>
-class utils::event<void(T0, T1, T2)>
-{
-public:
-    template <typename Functor> utils::event<void(T0, T1, T2)>& operator+= (Functor functor);
-    void operator()(T0, T1, T2) const;
-    operator bool() const;
-    int size() const { return functions_.size(); }
-
-private:
-    typedef std::list<utils::function<void(T0, T1, T2)> > Container;
-    Container functions_;
-};
-
-// -----------------------------------------------------------------------------
-
-template <typename T0, typename T1, typename T2, typename T3>
-class utils::event<void(T0, T1, T2, T3)>
-{
-public:
-    template <typename Functor> utils::event<void(T0, T1, T2, T3)>& operator+= (Functor functor);
-    void operator()(T0, T1, T2, T3) const;
-    operator bool() const;
-
-private:
-    std::deque<utils::function<void(T0, T1, T2, T3)> > functions_;
-};
-
-// -----------------------------------------------------------------------------
-
-template <typename Functor>
-inline utils::event<void()>&
-utils::event<void()>::operator+= (Functor functor)
-{
-    functions_.push_back(utils::function<void()>(functor));
-    return *this;
 }
-
-inline utils::event<void()>::operator bool() const
-{
-    return !functions_.empty();
-}
-
-inline void
-utils::event<void()>::operator()() const
-{
-    for (std::deque<utils::function<void()> >::const_iterator it(functions_.begin()), end(functions_.end());
-         it != end; ++it)
-    {
-        (*it)();
-    }
-}
-
-// -----------------------------------------------------------------------------
-
-template <typename T0>
-template <typename Functor>
-utils::event<void(T0)>&
-utils::event<void(T0)>::operator+= (Functor functor)
-{
-    functions_.push_back(utils::function<void(T0)>(functor));
-    return *this;
-}
-
-template <typename T0>
-utils::event<void(T0)>::operator bool() const
-{
-    return !functions_.empty();
-}
-
-template <typename T0>
-void
-utils::event<void(T0)>::operator()(T0 a0) const
-{
-    for (typename std::deque<utils::function<void(T0)> >::const_iterator it(functions_.begin()), end(functions_.end());
-         it != end; ++it)
-    {
-        (*it)(a0);
-    }
-}
-
-// -----------------------------------------------------------------------------
-
-template <typename T0, typename T1>
-template <typename Functor>
-utils::event<void(T0, T1)>&
-utils::event<void(T0, T1)>::operator+= (Functor functor)
-{
-    functions_.push_back(utils::function<void(T0, T1)>(functor));
-    return *this;
-}
-
-template <typename T0, typename T1>
-utils::event<void(T0, T1)>::operator bool() const
-{
-    return !functions_.empty();
-}
-
-template <typename T0, typename T1>
-void
-utils::event<void(T0, T1)>::operator()(T0 a0, T1 a1) const
-{
-    for (typename std::deque<utils::function<void(T0, T1)> >::const_iterator it(functions_.begin()),
-             end(functions_.end()); it != end; ++it)
-    {
-        (*it)(a0, a1);
-    }
-}
-
-// -----------------------------------------------------------------------------
-
-template <typename T0, typename T1, typename T2>
-template <typename Functor>
-utils::event<void(T0, T1, T2)>&
-utils::event<void(T0, T1, T2)>::operator+= (Functor functor)
-{
-    functions_.push_back(utils::function<void(T0, T1, T2)>(functor));
-    return *this;
-}
-
-template <typename T0, typename T1, typename T2>
-utils::event<void(T0, T1, T2)>::operator bool() const
-{
-    return !functions_.empty();
-}
-
-template <typename T0, typename T1, typename T2>
-void
-utils::event<void(T0, T1, T2)>::operator()(T0 a0, T1 a1, T2 a2) const
-{
-    for (typename Container::const_iterator it(functions_.begin()),
-             end(functions_.end()); it != end; ++it)
-    {
-        (*it)(a0, a1, a2);
-    }
-}
-
-// -----------------------------------------------------------------------------
-
-template <typename T0, typename T1, typename T2, typename T3>
-template <typename Functor>
-utils::event<void(T0, T1, T2, T3)>&
-utils::event<void(T0, T1, T2, T3)>::operator+= (Functor functor)
-{
-    functions_.push_back(utils::function<void(T0, T1, T2, T3)>(functor));
-    return *this;
-}
-
-template <typename T0, typename T1, typename T2, typename T3>
-utils::event<void(T0, T1, T2, T3)>::operator bool() const
-{
-    return !functions_.empty();
-}
-
-template <typename T0, typename T1, typename T2, typename T3>
-void
-utils::event<void(T0, T1, T2, T3)>::operator()(T0 a0, T1 a1, T2 a2, T3 a3) const
-{
-    for (typename std::deque<utils::function<void(T0, T1, T2, T3)> >::const_iterator it(functions_.begin()),
-             end(functions_.end()); it != end; ++it)
-    {
-        (*it)(a0, a1, a2, a3);
-    }
-}
-
-// -----------------------------------------------------------------------------
 
 #endif /* UTILS_EVENT_HPP */
