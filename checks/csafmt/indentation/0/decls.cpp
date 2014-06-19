0441eb38e6dc74ede32fc2ed7b62f83f92c1b28d
Hyman Rosen <hrosen4@bloomberg.net>
2014-06-19 13:47:09 -0400

    Various fixes for multi-declaration alignment.

--- checks/csafmt/indentation/0/decls.cpp
+++ checks/csafmt/indentation/0/decls.cpp
@@ -43,3 +43,35 @@ struct x
     int     qqqqqqqq;
     double& rrrrrrrrrr = ooooooooo;
 };
+
+char aaaa;
+int bb;
+
+double ddddd;
+float ee;
+unsigned gggggg;
+
+int hhhhhhhhhh;
+int iii;
+
+int kkkkkkk, llllll;
+
+char *mmmm;
+int nnnnnn;
+double& ooooooooo = ddddd;
+
+char   *pppppppppp;
+int     qqqqqqqq;
+double& rrrrrrrrrr = ooooooooo;
+
+void g()
+{
+    for (int i = 0; i < 10; ++i) {
+        volatile int j = i * i;
+    }
+
+    struct xxx { };
+    double d;
+    class z { } a;
+    int y;
+}
