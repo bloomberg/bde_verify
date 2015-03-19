47a3b42be08e612875dc910bf726dc37e822844f
Hyman Rosen <hrosen4@bloomberg.net>
2015-03-19 16:34:07 -0400

    Add TR11 checks.

---
+++ checks/csatr/global-type-only-in-source/0/gggx_ggg.h
@@ -0,0 +1,14 @@
+// gggx_ggg.h                                                         -*-C++-*-
+#ifndef INCLUDED_GGGX_GGG
+#define INCLUDED_GGGX_GGG
+
+namespace BloombergLP {
+namespace gggx {
+class Cc;
+class Dd;
+typedef int Ff;
+template <class PP> struct Tt;
+}  // close package namespace
+}  // close enterprise namespace
+
+#endif
