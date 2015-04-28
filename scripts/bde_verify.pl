a555ff8fbb7cc7a2b4777933443691e74c6787ba
Hyman Rosen <hrosen4@bloomberg.net>
2015-04-28 17:58:09 -0400

    Update Windows build and wrapper scripts.

--- scripts/bde_verify.pl
+++ scripts/bde_verify.pl
@@ -1,19 +1,33 @@
 #!/usr/bin/env perl
-# bdeverify_win                                                      -*-perl-*-
+# bde_verify.pl                                                      -*-perl-*-
 
 # Bde_verify is a static analysis tool that verifies that source code adheres
-# to the BDE coding standards.  Many of the checks are specific to Bloomberg LP
-# and this wrapper script contains default options for the Bloomberg LP
-# environment.
+# to the BDE coding standards.  This version of the wrapper script is for
+# Microsoft Windows.
 
 use strict;
 use warnings;
 use Getopt::Long;
+use Cwd;
 
-my $home     = "w:\\bde_verify";
-my $bb       = "c:\\BPCDEVTOOLS\\bde\\2.19.0";
-my $config   = "$home\\bde_verify.cfg";
-my $exe      = "c:\\bde-verify\\Release\\BDE_Verify.exe";
+my $bb       = $ENV{BDE_ROOT};
+$bb          = '' unless -d $bb;
+
+my $me       = Cwd::abs_path(${0});
+my $pt       = $me;
+$pt          =~ s{(bin/|scripts/)?[^/]*$}{};
+my $nm       = ${0};
+$nm          =~ s{.*[/\\]}{};
+
+my $config   = "${pt}etc/bde-verify/${nm}.cfg";
+$config      = "${pt}${nm}.cfg"                    unless -r $config;
+
+my $enm      = "bde_verify_bin";
+my $exe      = "${pt}libexec/bde-verify/$enm.exe";
+$exe         = "${pt}$enm.exe"                     unless -x $exe;
+$exe         = "$enm.exe"                          unless -x $exe;
+
+my @cl;
 my $debug    = "";
 my $verbose  = "";
 my $help     = "";
@@ -24,121 +38,177 @@ my @defs;
 my @wflags;
 my @lflags = (
     "cxx-exceptions",
-    "diagnostics-show-name",
+    "exceptions",
+    "diagnostics-show-note-include-stack",
     "diagnostics-show-option",
     "ms-compatibility",
     "ms-extensions",
-    "msc-version=1800",
-    #"delayed-template-parsing",
+   #"msc-version=1800",
+   #"delayed-template-parsing",
 );
 my $dummy;
 my @dummy;
 my $std;
 my @std;
-my $tlonly;
+my $warnoff;
+my $diagnose = "component";
+my $rwd;
+my $rwf;
+my $tag;
+my $ovr = -1;
+my $m32;
+my $m64;
+
+my $command = join " \\\n ", $0, map { join '" "', split(/ /, $_, -1) } @ARGV;
 
 sub usage()
 {
     print "
-    usage: $0 [options] [additional compiler options] file.cpp ...
-        -I{directory}
-        -D{macro}
-        -W{warning}
-        -f{flag}
-        --config=config_file     [$config]  
-        --bb=dir                 [$bb]
-        --exe=BDE_Verify.exe     [$exe]
-        --[no]definc             [$definc]
-        --[no]defdef             [$defdef]
-        --toplevel
-        --std=type
-        --debug
-        --verbose
-        --help
+usage: $0 [options] [additional compiler options] file.cpp ...
+    -I{directory}
+    -D{macro}
+    -w                       # disable normal compiler warnings
+    -W{warning}
+    -f{flag}
+    --config=config_file     [$config]  
+    --cl=config_line
+    --bb=dir                 [$bb]
+    --exe=binary             [$exe]
+    --[no]definc             [$definc]
+    --[no]defdef             [$defdef]
+    --[no]ovr                # whether to define BSL_OVERRIDES_STD
+    --rewrite-dir=dir
+    --rewrite-file=file
+    --diagnose={main,component,nogen,all}
+    --std=type
+    --tag=string
+    --debug
+    --verbose
+    --help
 
+For full documentation, see {TEAM BDE:BDE_VERIFY<GO>} or
+<http://cms.prod.bloomberg.com/team/display/bde/bde_verify>.
+
+Invoked as:
+$command
 ";
-    print "Invoked as\n", join(" \\\n", @ARGV), "\n";
     exit(1);
 }
 
 @ARGV = map { /^(-+[DIOWf])(.+)/ ? ($1, $2) : $_ } @ARGV;
 
 GetOptions(
-    'bb=s'       => \$bb,
-    'config=s'   => \$config,
-    'debug'      => \$debug,
-    'help|?'     => \$help,
-    'exe=s'      => \$exe,
-    'verbose'    => \$verbose,
-    'definc!'    => \$definc,
-    'defdef!'    => \$defdef,
-    "I=s"        => \@incs,
-    "D=s"        => \@defs,
-    "W=s"        => \@wflags,
-    "f=s"        => \@lflags,
-    "std=s"      => \$std,
-    "toplevel"   => \$tlonly,
-    "m32|m64|pipe|pthread|MMD|g|c" => \$dummy,
-    "O|MF|o=s"   => \@dummy,
+    'bb=s'                         => \$bb,
+    'config=s'                     => \$config,
+    'cl=s'                         => \@cl,
+    'debug'                        => \$debug,
+    'help|?'                       => \$help,
+    'cc=s'                         => \$dummy,
+    'exe=s'                        => \$exe,
+    'verbose|v'                    => \$verbose,
+    'definc!'                      => \$definc,
+    'defdef!'                      => \$defdef,
+    'ovr!'                         => \$ovr,
+    'nsa!'                         => \$dummy,
+    'rewrite|rewrite-dir|rd=s'     => \$rwd,
+    'rewrite-file|rf=s'            => \$rwf,
+    'w'                            => \$warnoff,
+    "I=s"                          => \@incs,
+    "D=s"                          => \@defs,
+    "W=s"                          => \@wflags,
+    "f=s"                          => \@lflags,
+    "std=s"                        => \$std,
+    "toplevel"                     => sub { $diagnose = "main" },
+    "diagnose=s"                   => sub {
+        my %opts = ("main" => 1, "component" => 1, "nogen" => 1, "all" => 1);
+        die "Unknown value '$_[1]' for '-$_[0]='\n" unless $opts{$_[1]};
+        $diagnose = $_[1];
+    },
+    'tag=s'                        => \$tag,
+    'llvm=s'                       => \$dummy,
+    "m32"                          => \$m32,
+    "m64"                          => \$m64,
+    "pipe|pthread|MMD|g|c|S"       => \$dummy,
+    "O|MF|o|march|mtune=s"         => \@dummy,
 ) and !$help and $#ARGV >= 0 or usage();
 
-my @config = ("-plugin-arg-bde_verify", "config=$config") if $config ne "";
-my @debug  = ("-plugin-arg-bde_verify", "debug-on")       if $debug;
-my @tlo    = ("-plugin-arg-bde_verify", "toplevel-only-on") if $tlonly;
+my @config = ("-plugin-arg-bde_verify", "config=$config")    if $config ne "";
+my @debug  = ("-plugin-arg-bde_verify", "debug-on")          if $debug;
+my @tlo    = ("-plugin-arg-bde_verify", "diagnose=$diagnose");
+my @rwd    = ("-plugin-arg-bde_verify", "rewrite-dir=$rwd")  if $rwd;
+print "No such directory $rwd\n" if $rwd and ! -d $rwd;
+my @rwf    = ("-plugin-arg-bde_verify", "rewrite-file=$rwf") if $rwf;
+my @tag    = ("-plugin-arg-bde_verify", "tool=$tag")         if $tag;
+my %uf     = (
+             );
+@lflags = map { "-f$_" } grep { not exists $uf{$_} } @lflags;
+@wflags = map { "-W$_" } grep { not /,/ } @wflags;
+unshift @wflags, "-w" if $warnoff;
 
-@lflags = map { "-f$_" if $_ ne "no-strict-aliasing" } @lflags;
-@wflags = map { "-W$_" } @wflags;
-
+my $macro = ${nm};
+$macro =~ s{^([^.]*).*}{\U$1\E};
+push(@defs, ($macro));
 push(@defs, (
+    "BB_THREADED",
     "BDE_BUILD_TARGET_DBG",
     "BDE_BUILD_TARGET_EXC",
     "BDE_BUILD_TARGET_MT",
-    "_CRT_SECURE_NO_DEPRECATE",
-    "_SCL_SECURE_NO_DEPRECATE",
-    "NOMINMAX",
-    "NOGDI",
-    "_WIN32_WINNT=0x0500",
-    "WINVER=0x0500",
-    "DEBUG",
-    "_MT",
-    "BSLS_IDENT_OFF",
-    "BSL_OVERRIDES_STD",
-    "BDE_OMIT_INTERNAL_DEPRECATED",
-    "_STLP_USE_STATIC_LIB",
-    "_STLP_HAS_NATIVE_FLOAT_ABS",
     "BDE_DCL=",
+    "BDE_OMIT_INTERNAL_DEPRECATED",
+    "BSLS_IDENT_OFF",
     "BSL_DCL=",
+    "DEBUG",
+    "NOGDI",
+    "NOMINMAX",
+    "WINVER=0x0500",
+    "_CRT_SECURE_NO_DEPRECATE",
+    "_MT",
+    "_SCL_SECURE_NO_DEPRECATE",
+    "_STLP_HAS_NATIVE_FLOAT_ABS",
+    "_STLP_USE_STATIC_LIB",
+    "_WIN32_WINNT=0x0500",
 )) if $defdef;
+if ($ovr == 1 || $ovr != 0 && $defdef && !grep(m{\bb[sd]l[^/]*$}, @ARGV)) {
+    push(@defs, "BSL_OVERRIDES_STD");
+}
 @defs = map { "-D$_" } @defs;
 
 for (@ARGV) {
-    if (m{^(.*)[/\\].*$}) {
-        push(@incs, $1) unless grep($1, @incs);
+    if (! -f) {
+        warn "Cannot find file $_\n";
+    } elsif (m{^(.*)[/\\].*$}) {
+        push(@incs, $1) unless grep($_ eq $1, @incs);
     } else {
         push(@incs, ".") unless grep($_ eq ".", @incs);
     }
 }
 push(@incs, (
     "$bb/include",
-    "$bb/include/stlport",
-)) if $definc;
-push(@incs, (
-    "$bb\\include",
-    "$bb\\include\\bsl+stdhdrs",
-));
+    "$bb/include/bsl+stdhdrs",
+)) if $bb and $definc;
+for (@incs) { warn "cannot find directory $_\n" unless -d; }
+@incs = map { ( "-I", $_ ) }
+        map { $_ eq Cwd::cwd() ? "." : $_ }
+        map { Cwd::abs_path($_) }
+        grep { -d } @incs;
 
-my $inc = $ENV{INCLUDE};
-if ($inc ne '') {
-    push(@incs, split(/;/, $ENV{INCLUDE}));
-} else {
-    push(@incs, "c:\\Program Files (x86)" .
-                  "\\Microsoft Visual Studio 11.0" .
-                  "\\VC" .
-                  "\\include");
-}
-@incs = map { ("-I", $_, "-cxx-isystem", $_ ) } @incs;
+my @pass;
+push(@pass, "-m32") if $m32;
+push(@pass, "-m64") if $m64;
+
+my $inc = $ENV{INCLUDE} ||
+    "C:/Program Files (x86)/Microsoft Visual Studio 12.0/VC/INCLUDE;" .
+    "C:/Program Files (x86)/Microsoft Visual Studio 12.0/VC/ATLMFC/INCLUDE;" .
+    "C:/Program Files (x86)/Windows Kits/8.1/include/shared;" .
+    "C:/Program Files (x86)/Windows Kits/8.1/include/um;" .
+    "C:/Program Files (x86)/Windows Kits/8.1/include/winrt";
+push(@incs, map { ( "-isystem", $_ ) }
+            map { Cwd::abs_path($_) }
+            grep { -d } split(/;/, $inc));
 
 push(@std, "-std=$std") if $std;
+
+@cl = map { ("-plugin-arg-bde_verify", "config-line=$_") } @cl;
 
 my @command = (
     "$exe",
@@ -148,6 +218,10 @@ my @command = (
     @debug,
     @config,
     @tlo,
+    @rwd,
+    @rwf,
+    @tag,
+    @cl,
     @defs,
     @incs,
     @lflags,
@@ -155,7 +229,14 @@ my @command = (
     @ARGV,
 );
 
-print join(' ', @command), "\n" if $verbose;
+map { s{\\}{/}g } @command;
+
+if ($verbose)
+{
+    my @c = @command;
+    map { s{(.*)}{"$1"} if m{[( )]} and not m{"} } @c;
+    print join(" \\\n ", @c), "\n";
+}
 
 exec @command;
 
