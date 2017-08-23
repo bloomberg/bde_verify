97ccac8cb298e457ce971bd4549c172137b2e1c3
Hyman Rosen <hrosen4@bloomberg.net>
2017-08-23 14:49:37 -0400

    DRQS 105530463 fix indentation check when no braces.

--- groups/csa/csafmt/csafmt_indent.cpp
+++ groups/csa/csafmt/csafmt_indent.cpp
@@ -161,6 +161,12 @@ struct report : public RecursiveASTVisitor<report>, public Report<data>
     bool VisitFieldDecl(FieldDecl *fd);
     bool VisitVarDecl(VarDecl *vd);
 
+    bool WalkUpFromDoStmt(DoStmt *stmt);
+    bool WalkUpFromForStmt(ForStmt *stmt);
+    bool WalkUpFromIfStmt(IfStmt *stmt);
+    bool WalkUpFromSwitchStmt(SwitchStmt *stmt);
+    bool WalkUpFromWhileStmt(WhileStmt *stmt);
+
     void do_consecutive();
         // Issue warnings for misaligned declarators and clear the existing
         // declarator set.
@@ -214,6 +220,66 @@ bool report::VisitDecl(Decl *decl)
         }
     }
     return true;
+}
+
+bool report::WalkUpFromDoStmt(DoStmt *stmt)
+{
+    if (a.is_component(stmt) && !stmt->getDoLoc().isMacroID()) {
+        if (!llvm::dyn_cast<CompoundStmt>(stmt->getBody())) {
+            add_indent(stmt->getBody()->getLocStart(), +4);
+            add_indent(stmt->getWhileLoc(), -4);
+        }
+    }
+    return Base::WalkUpFromDoStmt(stmt);
+}
+
+bool report::WalkUpFromForStmt(ForStmt *stmt)
+{
+    if (a.is_component(stmt) && !stmt->getForLoc().isMacroID()) {
+        if (!llvm::dyn_cast<CompoundStmt>(stmt->getBody())) {
+            add_indent(stmt->getBody()->getLocStart(), +4);
+            add_indent(stmt->getLocEnd(), -4);
+        }
+    }
+    return Base::WalkUpFromForStmt(stmt);
+}
+
+bool report::WalkUpFromIfStmt(IfStmt *stmt)
+{
+    if (a.is_component(stmt) && !stmt->getIfLoc().isMacroID()) {
+        if (!llvm::dyn_cast<CompoundStmt>(stmt->getThen())) {
+            add_indent(stmt->getIfLoc().getLocWithOffset(2), +4);
+            add_indent(
+                stmt->getElse() ? stmt->getElseLoc() : stmt->getLocEnd(), -4);
+        }
+        if (stmt->getElse() && !llvm::dyn_cast<CompoundStmt>(stmt->getElse())) {
+            add_indent(stmt->getElseLoc().getLocWithOffset(4), +4);
+            add_indent(stmt->getLocEnd(), -4);
+        }
+    }
+    return Base::WalkUpFromIfStmt(stmt);
+}
+
+bool report::WalkUpFromSwitchStmt(SwitchStmt *stmt)
+{
+    if (a.is_component(stmt) && !stmt->getSwitchLoc().isMacroID()) {
+        if (!llvm::dyn_cast<CompoundStmt>(stmt->getBody())) {
+            add_indent(stmt->getBody()->getLocStart(), +4);
+            add_indent(stmt->getLocEnd(), -4);
+        }
+    }
+    return Base::WalkUpFromSwitchStmt(stmt);
+}
+
+bool report::WalkUpFromWhileStmt(WhileStmt *stmt)
+{
+    if (a.is_component(stmt) && !stmt->getWhileLoc().isMacroID()) {
+        if (!llvm::dyn_cast<CompoundStmt>(stmt->getBody())) {
+            add_indent(stmt->getBody()->getLocStart(), +4);
+            add_indent(stmt->getLocEnd(), -4);
+        }
+    }
+    return Base::WalkUpFromWhileStmt(stmt);
 }
 
 bool report::WalkUpFromEnumConstantDecl(EnumConstantDecl *decl)
