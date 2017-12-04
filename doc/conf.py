0d9b95f56e4143f9b0908fc5eb99c5a3fd127cf4
Hyman Rosen <hrosen4@bloomberg.net>
2017-12-04 17:48:09 -0500

    Up version to 1.2.18

--- doc/conf.py
+++ doc/conf.py
@@ -20,8 +20,8 @@ elif tags.has('bde_verify'):
 else:
     sys.exit("Specify -t bb_cppverify or -t bde_verify in SPHINXOPTS.")
 
-version           = '1.2.16'
-release           = '1.2.16'
+version           = '1.2.18'
+release           = '1.2.18'
 author            = u'Hyman Rosen'
 copyright         = u'2015, %s' % author
 
