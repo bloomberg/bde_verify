2fb5599eb25deb2c11e57c7e30fb151b5f0a545f
Hyman Rosen <hrosen4@bloomberg.net>
2019-02-08 13:35:42 -0500

    Update version to 1.3.1

--- doc/docs/conf.py
+++ doc/docs/conf.py
@@ -41,9 +41,9 @@ author = u'Hyman Rosen'
 copyright = u'2018, Bloomberg LP'
 
 # The short X.Y version
-version = u'1.2.26'
+version = u'1.3.1'
 # The full version, including alpha/beta/rc tags
-release = u'1.2.26'
+release = u'1.3.1'
 
 # -- General configuration ---------------------------------------------------
 
