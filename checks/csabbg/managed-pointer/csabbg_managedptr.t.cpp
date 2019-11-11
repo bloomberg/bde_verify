ad74674e3246b55e1135d0d2e1d3339d550a42b9
Hyman Rosen <hrosen4@bloomberg.net>
2019-11-11 11:48:09 -0500

    Add member initialization checks

--- checks/csabbg/managed-pointer/csabbg_managedptr.t.cpp
+++ checks/csabbg/managed-pointer/csabbg_managedptr.t.cpp
@@ -77,6 +77,46 @@ void f(bslma::Allocator *pa)
     ManagedPtr<char> ep08(new (*da) char, bslma::Default::allocator());
 }
 
+struct X {
+    bslma::Allocator *da, *ba;
+    bslma::TestAllocator ta;
+
+    ManagedPtr<char> d_mp01, d_mp02, d_mp03, d_mp04, d_mp05, d_mp06, d_mp07;
+    ManagedPtr<char> d_mp08, d_mp09, d_mp10, d_mp11, d_mp12, d_mp13, d_mp14;
+    ManagedPtr<char> d_mp15, d_mp16, d_mp17, d_mp18, d_mp19;
+    ManagedPtr<char> d_ep01, d_ep02, d_ep03, d_ep04, d_ep05, d_ep06;
+    ManagedPtr<char> d_ep07, d_ep08;
+    X(bslma::Allocator *pa)
+    : da(bslma::Default::allocator())
+    , ba(Default::allocator())
+    , d_mp01(new char)
+    , d_mp02(new (*pa) char)
+    , d_mp03(new (*pa) char, pa)
+    , d_mp04(new (*da) char)
+    , d_mp05(new (*da) char, da)
+    , d_mp06(new (*ba) char)
+    , d_mp07(new (*ba) char, ba)
+    , d_mp08(new (ta) char)
+    , d_mp09(new (ta) char, &ta)
+    , d_mp10(new (*bslma::Default::allocator()) char)
+    , d_mp11(new (*bslma::Default::allocator()) char,
+                          bslma::Default::allocator())
+    , d_mp12(new (*Default::allocator()) char)
+    , d_mp13(new (*Default::allocator()) char, Default::allocator())
+    , d_mp16(new (*ba) char)
+    , d_mp17(new (*ba) char, ba)
+    , d_ep01(new char, da)
+    , d_ep02(new (*ba) char, da)
+    , d_ep03(new (ta) char, ba)
+    , d_ep04(new (*da) char, &ta)
+    , d_ep05(new (*bslma::Default::allocator()) char, da)
+    , d_ep06(new (*Default::allocator()) char, da)
+    , d_ep07(new (*da) char, Default::allocator())
+    , d_ep08(new (*da) char, bslma::Default::allocator())
+    {
+    }
+};
+
 // ----------------------------------------------------------------------------
 // Copyright (C) 2019 Bloomberg Finance L.P.
 //
