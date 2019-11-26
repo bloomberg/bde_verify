2503141697d07824f6b4b3de8291053f641d55fd
Hyman Rosen <hrosen4@bloomberg.net>
2019-11-26 17:29:42 -0500

    Inc version

--- doc/docs/conf.py
+++ doc/docs/conf.py
@@ -41,9 +41,9 @@ author = u'Hyman Rosen'
 copyright = u'2018, Bloomberg LP'
 
 # The short X.Y version
-version = u'1.3.9'
+version = u'1.3.10'
 # The full version, including alpha/beta/rc tags
-release = u'1.3.9'
+release = u'1.3.10'
 
 # -- General configuration ---------------------------------------------------
 
