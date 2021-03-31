67f37e55b44f4cb8ff5b4f1abd96eb928bae94b5
Oleg Subbotin <osubbotin@bloomberg.net>
2021-03-31 13:28:29 -0400

    Bump version to 1.3.18

--- doc/docs/conf.py
+++ doc/docs/conf.py
@@ -41,9 +41,9 @@ author = u'Hyman Rosen'
 copyright = u'2018, Bloomberg LP'
 
 # The short X.Y version
-version = u'1.3.17'
+version = u'1.3.18'
 # The full version, including alpha/beta/rc tags
-release = u'1.3.17'
+release = u'1.3.18'
 
 # -- General configuration ---------------------------------------------------
 
